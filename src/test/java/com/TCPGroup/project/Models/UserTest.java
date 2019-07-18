5d50f70dbcdddd36db69bc0f90663f209f3203ae
bcorbett0312 <benjamin.t.corbett@gmail.com>
2019-07-18 08:19:28 -0400

    copied tests from old code

---
+++ src/test/java/com/TCPGroup/project/Models/UserTest.java
@@ -0,0 +1,68 @@
+package com.TCPGroup.project.Models;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+
+    private User user;
+
+    @Before
+    public void setup(){
+        this.user = new User();
+    }
+
+    @Test
+    public void getPassword() {
+    }
+
+    @Test
+    public void setPassword() {
+    }
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void getIdNull() {
+        Assert.assertNull(this.user.getId());
+    }
+
+    @Test
+    public void setId() {
+        Integer expected =1;
+        this.user.setId(expected);
+        Integer actual = this.user.getId();
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void getUserNameNull() {
+        Assert.assertNull(this.user.getUsername());
+    }
+
+    @Test
+    public void setUserName() {
+        String expected = "username";
+        this.user.setUsername(expected);
+        String actual = this.user.getUsername();
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void getAuthenticated() {
+        Assert.assertNull(this.user.getAuthenticated());
+    }
+
+    @Test
+    public void setAuthenticated() {
+        Boolean expected =false;
+        this.user.setAuthenticated(expected);
+        Boolean actual = this.user.getAuthenticated();
+        Assert.assertEquals(expected,actual);
+    }
+}
