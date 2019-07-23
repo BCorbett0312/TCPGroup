8d9b2258bb7c580ac99c304576ecafc05a453479
Dandridge Cole <dandridgecole@yahoo.com>
2019-07-23 11:34:02 -0400

    Update env.service.ts

--- src/main/resources/frontend/tcpproject/src/app/env.service.ts
+++ src/main/resources/frontend/tcpproject/src/app/env.service.ts
@@ -4,7 +4,7 @@ export class EnvService {
   // be overridden by env.js
 
   // API url
-  public apiUrl = 'https://teamac.herokuapp.com';
+  public apiUrl = 'https://teamac.herokuapp.com'; // 'http://localhost:8080'; //
 
   // Whether or not to enable debug mode
   public enableDebug = true;
