6e130fcadd2827448b389dd3b91d254f1c254e6b
Dandridge Cole <dandridgecole@yahoo.com>
2019-08-01 08:46:47 -0400

    basic subscription service tests

---
+++ src/test/java/com/TCPGroup/project/Controllers/ChannelControllerTest.java
@@ -0,0 +1,36 @@
+package com.TCPGroup.project.Controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ChannelControllerTest {
+
+    @Test
+    public void getAllChannels() {
+    }
+
+    @Test
+    public void getChannelById() {
+    }
+
+    @Test
+    public void getChannelIdsByUserId() {
+    }
+
+    @Test
+    public void getChannelsByUserId() {
+    }
+
+    @Test
+    public void createNewChannel() {
+    }
+
+    @Test
+    public void getStandardChannels() {
+    }
+
+    @Test
+    public void createDirectChannel() {
+    }
+}
---
+++ src/test/java/com/TCPGroup/project/Services/ChannelServiceTest.java
@@ -0,0 +1,40 @@
+package com.TCPGroup.project.Services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ChannelServiceTest {
+
+    @Test
+    public void getAllChannels() {
+    }
+
+    @Test
+    public void getChannelById() {
+    }
+
+    @Test
+    public void createChannel() {
+    }
+
+    @Test
+    public void getStandardChannels() {
+    }
+
+    @Test
+    public void getChannelsByIds() {
+    }
+
+    @Test
+    public void getChannelIdsByUserId() {
+    }
+
+    @Test
+    public void getChannelsByUserId() {
+    }
+
+    @Test
+    public void createDirectChannel() {
+    }
+}
