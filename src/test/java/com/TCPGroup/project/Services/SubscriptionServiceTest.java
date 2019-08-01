2903497faf5df09156e7dd1a18c09feb508d939c
Dandridge Cole <dandridgecole@yahoo.com>
2019-08-01 08:31:06 -0400

    rudimentatry sanitization for message body; also subscription controller tests

---
+++ src/test/java/com/TCPGroup/project/Services/SubscriptionServiceTest.java
@@ -0,0 +1,24 @@
+package com.TCPGroup.project.Services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SubscriptionServiceTest {
+
+    @Test
+    public void getSubscriptionsByUserId() {
+    }
+
+    @Test
+    public void getSubscriptionsByChannelId() {
+    }
+
+    @Test
+    public void createSubscription() {
+    }
+
+    @Test
+    public void createSubscriptions() {
+    }
+}
